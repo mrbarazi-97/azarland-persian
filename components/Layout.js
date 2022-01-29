e26014edf9553ff229a6dfa5d91ef35efbbd4629
mrbarazi-97 <mrbarazi.97@gmail.com>
2022-01-30 01:22:10 +0330

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,115 @@
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Link,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  Switch,
+} from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
+import Head from 'next/head';
+import useStyles from '../utils/style';
+import NextLink from 'next/link';
+import Image from 'next/image';
+import { Store } from '../utils/Store';
+import Cookies from 'js-cookie';
+import React, { useContext } from 'react';
+//import Iran from './fonts/Iran.woff2';
+
+export default function Layout({ description, title, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Vazir',
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+          @font-face {
+            font-family: 'Vazir';
+            font-style: normal;
+            font-display: swap;
+            font-weight: 400;
+            src: local('Vazir'), local('Vazir'), url('./fonts/Vazir-Medium.woff2') format('woff2');
+            unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+          }
+        `,
+      },
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      light: {
+        main: '#ffffff',
+      },
+      primary: {
+        main: '#8b0000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
+  const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
+  return (
+    <div>
+      <Head>
+        <title>{title ? `${title} - آذرلند ` : ` آذرلند`}</title>
+        {description && <meta name="description" content={description}></meta>}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar dir="rtl" position="static" className={classes.navbar}>
+          <Toolbar className={classes.Htoolbar}>
+            <NextLink href="/" passHref>
+              <Link>
+                <Image
+                  className={classes.logo}
+                  src="/Azarland Logo.png"
+                  alt="azarland logo"
+                  width={125}
+                  height={75}
+                />
+              </Link>
+            </NextLink>
+            <Switch
+              checked={darkMode}
+              onChange={darkModeChangeHandler}
+            ></Switch>
+            <div dir="ltr">
+              <NextLink href="/login" passHref>
+                <Link>ورود</Link>
+              </NextLink>
+              <NextLink href="/card" passHref>
+                <Link>سبد خرید </Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container dir="rtl" className={classes.main}>
+          {children}
+        </Container>
+        <footer className={classes.footer}>
+          <Typography>Created by Baraz.one | 2022</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
---
+++ pages/product/[slug].js
@@ -0,0 +1,121 @@
+import React from 'react';
+//import { useRouter } from 'next/router';
+//import data from '../../utils/data';
+import Layout from '../../components/Layout';
+import NextLink from 'next/link';
+import {
+  Grid,
+  Link,
+  List,
+  ListItem,
+  Typography,
+  Card,
+  Button,
+} from '@material-ui/core';
+import useStyles from '../../utils/style';
+import Image from 'next/image';
+import db from '../utils/db';
+import Product from '../models/Product';
+
+export default function ProductScreen(props) {
+  const {product} = props;
+  const classes = useStyles();
+  //const router = useRouter();
+  //const { slug } = router.query;
+  //const product = data.products.find((a) => a.slug === slug);
+  if (!product) {
+    return <div>محصول مورد نظر پیدا نشد </div>;
+  }
+  return (
+    <Layout title={product.name} description={product.description}>
+      <div className={classes.section}>
+        <NextLink href="/" passHref>
+          <Link>
+            <Typography>بازگشت به محصولات</Typography>
+          </Link>
+        </NextLink>
+      </div>
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <List>
+            <ListItem>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>دسته بندی : {product.category}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>برند : {product.brand}</Typography>
+            </ListItem>
+            <ListItem>
+              <Typography>
+                امتیاز : {product.rating} ستاره ({product.numReviews} بازدید)
+              </Typography>
+            </ListItem>
+            <ListItem>
+              توضیحات :<Typography>{product.description}</Typography>
+            </ListItem>
+          </List>
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Card>
+            <List>
+              <ListItem className={classes.state}>
+                <Grid container>
+                  <Grid item xs={1}>
+                    <Typography>قیمت</Typography>
+                  </Grid>
+                  <Grid item xs={7}>
+                    <Typography>{product.price} تومن</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem className={classes.state}>
+                <Grid container>
+                  <Grid item xs={1}>
+                    <Typography>وضعیت</Typography>
+                  </Grid>
+                  <Grid item xs={7}>
+                    <Typography>
+                      {product.countInStock > 0 ? 'موجود' : 'ناموجود'}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
+              <ListItem>
+                <Button fullWidth variant="contained" color="primary">
+                  افزودن به سبد خرید
+                </Button>
+              </ListItem>
+            </List>
+          </Card>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: db.convertDocToObj(product),
+    },
+  };
+}
